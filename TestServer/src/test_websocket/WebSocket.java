ce4d9785e07fad167f4662886fece8c7ae940645
user <user@soba>
2019-07-10 13:38:49 +0900

    aa

---
+++ TestServer/src/test_websocket/WebSocket.java
@@ -0,0 +1,37 @@
+package test_websocket;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+
+@ServerEndpoint("/WebSocketServer")
+public class WebSocket
+{
+	@OnOpen
+    public void onOpen(Session session) {
+        /* セッション確立時の処理 */
+        System.out.println("WebSocketセッション確立");
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        /* メッセージ受信時の処理 */
+        System.out.println("WebSocket受信：" + message);
+    }
+
+    @OnError
+    public void onError(Throwable th) {
+        /* エラー発生時の処理 */
+        System.out.println("WebSocketエラー：" + th.getMessage());
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        /* セッション解放時の処理 */
+        System.out.println("WebSocketセッション確立");
+    }
+}
