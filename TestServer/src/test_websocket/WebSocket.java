0148701ab892b27ab546dde8af88682e9bbf7ea1
user <user@soba>
2019-07-11 16:24:42 +0900

    aa

--- TestServer/src/test_websocket/WebSocket.java
+++ TestServer/src/test_websocket/WebSocket.java
@@ -1,38 +1,41 @@
 package test_websocket;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+@ServerEndpoint(value = "/TestServer")
+public class WebSocket{
+    //notice:not thread-safe
+    private static ArrayList<Session> sessionList = new ArrayList<Session>();
 
-@ServerEndpoint("/WebSocketServer")
-public class WebSocket
-{
-
-	@OnOpen
-    public void onOpen(Session session) {
-        /* セッション確立時の処理 */
-		System.out.println(session.getId() + " was connected.");
-    }
-
-    @OnMessage
-    public void onMessage(String message) {
-        /* メッセージ受信時の処理 */
-        System.out.println("WebSocket受信：" + message);
-    }
-
-    @OnError
-    public void onError(Throwable th) {
-        /* エラー発生時の処理 */
-        System.out.println("WebSocketエラー：" + th.getMessage());
+    //Session確立 ハンドシェイクするらしい
+    @OnOpen
+    public void onOpen(Session session){
+        try{
+            sessionList.add(session);
+            //asynchronous communication
+            session.getBasicRemote().sendText("Hello!");
+        }catch(IOException e){}
     }
 
     @OnClose
-    public void onClose(Session session) {
-        /* セッション解放時の処理 */
-        System.out.println("WebSocketセッション確立");
+    public void onClose(Session session){
+        sessionList.remove(session);
+    }
+
+    @OnMessage
+    public void onMessage(String msg){
+        try{
+            for(Session session : sessionList){
+                //asynchronous communication
+                session.getBasicRemote().sendText(msg);
+            }
+        }catch(IOException e){}
     }
 }
